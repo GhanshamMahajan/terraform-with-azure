9ce51ba2f85d40ee9cf9bb4556f87b37c8b584e9
Ghansham Mahajan <51263352+GhanshamMahajan@users.noreply.github.com>
2023-07-15 06:15:07 +0530

    Create main.tf

---
+++ linux_webapp/main.tf
@@ -0,0 +1,21 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_service_plan" "asp" {
+  name                = var.service_plan_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = var.os_type
+  sku_name            = var.sku_name
+}
+
+resource "azurerm_linux_web_app" "alwp" {
+  name                = var.linux_web_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.asp.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {}
+}
